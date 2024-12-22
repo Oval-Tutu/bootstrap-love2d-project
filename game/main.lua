1e017b20baf61dbd3be8bc629a5be022aa50fe52
Martin Wimpress <martin@wimpress.org>
2024-12-22 19:15:28 +0000

    feat: update main.lua with a example game

--- game/main.lua
+++ game/main.lua
@@ -1,9 +1,24 @@
 function love.load()
-end
-
-function love.update()
-	print("Hello!")
+  love.graphics.setFont(love.graphics.newFont(42))
+  x, y = 0, 0
 end
 
 function love.draw()
+  local message = "Mouse (" .. x .. "," .. y .. ")"
+
+  -- Center the text on the screen
+  local font = love.graphics.getFont()
+  local textWidth = font:getWidth(message)
+  local centerX = (love.graphics.getWidth() / 2) - (textWidth / 2)
+
+  -- Draw a red circle at the mouse's position and the message over it.
+  love.graphics.setColor(1, 0, 0)
+  love.graphics.circle("fill", x, y, 10)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.print(message, centerX, love.graphics.getHeight() / 2)
+end
+
+function love.update(dt)
+  -- Gets the x- and y-position of the mouse.
+  x, y = love.mouse.getPosition()
 end
