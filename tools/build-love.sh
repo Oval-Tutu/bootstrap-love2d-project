44f0773e12bb44f39620529e32a18c1d72830427
Martin Wimpress <martin@wimpress.org>
2024-12-24 14:37:39 +0000

    refactor: update build-love.sh to use 7z
    Also date stamps the .love files, so it also doubles up as a poor mans  backup system.

--- tools/build-love.sh
+++ tools/build-love.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 PACKAGE_NAME="Game";
+# If $1 is set use it as the package name
 if [ -n "${1}" ]; then
   PACKAGE_NAME="${1}"
 fi
 
-#If $2 is set use it as the package name suffix
+# If $2 is set use it as the package name suffix
 if [ -n "${2}" ]; then
   PACKAGE_NAME="${PACKAGE_NAME}-${2}"
+else
+  PACKAGE_NAME="${PACKAGE_NAME}-$(date +%y.%j.%H%M)"
 fi
 
 # Create love package using zip
-if command -v zip &>/dev/null; then
-  if [ -e "builds/${PACKAGE_NAME}.love" ]; then
-    rm "builds/${PACKAGE_NAME}.love"
-  fi
-  (cd game && zip -9 -r "../builds/${PACKAGE_NAME}.love" . -x "*.gitkeep")
+if command -v 7z &>/dev/null; then
+  7z a -tzip ./builds/"${PACKAGE_NAME}.love" ./game/* -xr!.gitkeep
 else
-  echo "ERROR! Command not found: zip"
+  echo "ERROR! Command not found: 7z"
   exit 1
 fi
